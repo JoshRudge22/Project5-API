6b940430fb607afaab47c2577f154e955614a5b6
Josh Rudge <joshrudge@hotmail.com>
2024-06-03 09:25:11 +0000

    added the get to my api_view for my contact app. I can now see the contact forms stored in the backends browser

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,10 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Contact
 from .serializers import ContactSerializer
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def contact(request):
     if request.method == 'POST':
         serializer = ContactSerializer(data=request.data)
@@ -12,3 +12,7 @@ def contact(request):
             serializer.save()
             return Response({'message': 'Form submitted successfully!'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        contacts = Contact.objects.all()
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
