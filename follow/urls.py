9c3737998240fd72e04326df5bdca4d253c71c67
Josh Rudge <joshrudge@hotmail.com>
2024-06-04 11:20:33 +0000

    in the following app I created a view where the user can see who other users are following or getting follwed by

--- follow/urls.py
+++ follow/urls.py
@@ -4,4 +4,6 @@ from follow import views
 urlpatterns = [
     path('followers/', views.FollowerList.as_view()),
     path('followers/<int:pk>/', views.FollowerDetail.as_view()),
+    path('user/<int:user_id>/followers/', views.UserFollowersList.as_view()),
+    path('user/<int:user_id>/following/', views.UserFollowingList.as_view()),
 ]
--- follow/views.py
+++ follow/views.py
@@ -15,3 +15,17 @@ class FollowerDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
+
+class UserFollowersList(generics.ListAPIView):
+    serializer_class = FollowerSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Follower.objects.filter(user_id=user_id)
+
+class UserFollowingList(generics.ListAPIView):
+    serializer_class = FollowerSerializer
+
+    def get_queryset(self):
+        follower_id = self.kwargs['user_id']
+        return Follower.objects.filter(follower_id=follower_id)
