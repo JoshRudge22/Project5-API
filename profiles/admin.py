fff5531ade0972af2c3fa4781942f1b3670b3a60
Josh Rudge <joshrudge@hotmail.com>
2024-06-10 09:21:43 +0000

    updated the profile model as the new users were not showing in the admin panel under profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'username', 'full_name', 'created_at')
+    search_fields = ('user__username', 'username', 'full_name')
