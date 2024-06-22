2e6accc904970eb19ede56e0334bc5973f7dd876
Josh Rudge <joshrudge@hotmail.com>
2024-06-22 14:18:36 +0000

    removed all video code from the posts app, everytime I created a video the broswer was aying I needed to upgrade my plan

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,7 +7,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'user', 'image', 'video', 'caption', 'created_at', 'owner', 'profile_image']
+        fields = ['id', 'user', 'image', 'caption', 'created_at', 'owner', 'profile_image']
         read_only_fields = ['id', 'user', 'created_at']
 
     def get_profile_image(self, obj):
@@ -15,14 +15,10 @@ class PostSerializer(serializers.ModelSerializer):
             return obj.user.profile.profile_image.url
         return None
 
-    def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.width > 4096 or value.image.height > 4096:
-            raise serializers.ValidationError('Image dimensions larger than 4096px!')
-        return value
-
-    def validate_video(self, value):
-        if value.size > 1024 * 1024 * 100:
-            raise serializers.ValidationError('Video size larger than 100MB!')
-        return value
+    def validate(self, data):
+        if 'image' not in data or not data['image']:
+            raise serializers.ValidationError('You must upload an image')
+        if 'image' in data and data['image']:
+            if data['image'].size > 10485760:
+                raise serializers.ValidationError('Image size should not exceed 10MB')
+        return data
