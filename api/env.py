6ba68d8519d8f284fde3c98e6f8e359fa9a30113
JoshRudge22 <139856712+JoshRudge22@users.noreply.github.com>
2024-10-01 00:09:36 +0500

    Create env.py

---
+++ api/env.py
@@ -0,0 +1,7 @@
+# env.py
+import os
+
+os.environ['SECRET_KEY'] = 'your_secret_key_here'
+os.environ['CLOUDINARY_URL'] = 'your_cloudinary_url_here'
+os.environ['ALLOWED_HOST'] = 'your_allowed_host_here'  # e.g., 'localhost' or your production domain
+# Add any other necessary environment variables here
