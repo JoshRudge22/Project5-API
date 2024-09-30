c4eaf9e41f75e87219eb0812de933a9bbed6ad61
JoshRudge22 <139856712+JoshRudge22@users.noreply.github.com>
2024-10-01 00:15:08 +0500

    Update admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -5,3 +5,4 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'username', 'full_name', 'created_at')
     search_fields = ('user__username', 'username', 'full_name')
+SITE_ID = 1  # This should match your site's ID in the Sites admin panel
