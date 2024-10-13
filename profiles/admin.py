1f55a5842157e9524496c3dc1870bc923b58764f
Josh Rudge <joshrudge@hotmail.com>
2024-10-13 19:52:18 +0000

    Fix configuration and dependencies

--- profiles/admin.py
+++ profiles/admin.py
@@ -5,12 +5,3 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'username', 'full_name', 'created_at')
     search_fields = ('user__username', 'username', 'full_name')
-<<<<<<< Updated upstream
-
-=======
-<<<<<<< HEAD
-SITE_ID = 1  # This should match your site's ID in the Sites admin panel
-=======
-
->>>>>>> cc7f036 (updated files)
->>>>>>> Stashed changes
